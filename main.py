c29c3ad148cb7de1f073a8551642f278a2c85d4e
brnakin <brnakinn@gmail.com>
2024-12-17 01:10:30 +0100

    Some changes about Chrome Options
    - Changed to '--headless' options to improve speed
    - Fixed the webdriver regarding to Chrome Options

--- main.py
+++ main.py
@@ -3,46 +3,77 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, ElementNotInteractableException
+from selenium.webdriver.chrome.options import Options
+
 import time
 
+# Setup Chrome options
+chrome_options = Options()
+
+# Headless chrome for less overhead and faster
+chrome_options.add_argument("--headless")
 
 # Path to chromedriver
-chromedriver_path = (
-    "/usr/local/bin/chromedriver"  # Update this with the actual path to chromedriver
-)
+chromedriver_path = "/usr/local/bin/chromedriver"
 
 # Setup the Service
 service = Service(executable_path=chromedriver_path)
 
 # Initialize the WebDriver with Service and Chrome options
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Set window size
-driver.set_window_size(1320, 768)
+driver.set_window_size(1920, 1080)
 
-# Navigate to the page
-ico_link = "https://icodrops.com/category/ended-ico/"
-driver.get(ico_link)
+try:
+    # Navigate to the page
+    ico_link = "https://icodrops.com/category/ended-ico/"
+    driver.get(ico_link)
 
-time.sleep(5)
+    # Wait and click 'Agree' button
+    try:
+        consent_button = WebDriverWait(driver, 30).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "//button[contains(text(), 'Agree') or contains(text(), 'Accept')]",
+                )
+            )
+        )
+        consent_button.click()
+    except TimeoutException:
+        print("Consent button not found or not clickable")
 
-# In order to click 'Show more' button' first we need to click 'Agree' button.
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "/html/body/footer/div/div[3]/div/button"))
-)
-show_more = driver.find_element(By.XPATH, "/html/body/footer/div/div[3]/div/button")
-show_more.click()
+    time.sleep(5)
 
-time.sleep(5)
+    # Repeatedly click 'Show more' button
+    while True:
+        try:
+            # Wait up to 30 seconds for the 'Show more' button to be present and visible
+            show_more_button = WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "//button[contains(text(), 'Show more')]")
+                )
+            )
 
-# In order to click 'Show more' button.
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/button"))
-)
-show_more = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/button")
-show_more.click()
+            try:
+                # Attempt to click the button and wait for 5 seconds
+                show_more_button.click()
+                time.sleep(5)
+            except ElementNotInteractableException:
+                # If the button is found but not clickable, break the loop
+                print("Finished loading content.")
+                break
 
-time.sleep(5)
+        except TimeoutException:
+            # If the button is not found within 30 seconds, assume content is fully loaded
+            print("No more 'Show more' button found.")
+            break
 
-# Always remember to close the driver when you're done
-driver.quit()
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+finally:
+    # Always close the driver
+    driver.quit()
